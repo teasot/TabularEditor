17865280742938bb60f7eba54968448acfe5b8e1
Anthony Flynn <8462929+teasot@users.noreply.github.com>
2018-04-27 22:15:38 +1000

    Added TOM2AMO files to Tabular Editor
    Added TOM2AMO files to Tabular Editor, as a folder. This is easier than referencing the DLL.

---
+++ TOMWrapper/TOM2AMO/File/AMOFile.cs
@@ -0,0 +1,46 @@
+﻿using AMO = Microsoft.AnalysisServices;
+namespace TOM2AMO.File
+{
+    public static class AMOFile
+    {
+        /// <summary>
+        /// Reads an 1103 or 1100 Database file from the provided file location
+        /// </summary>
+        /// <param name="FileLocation">The location of the file to be loaded. Usually a .bim file.</param>
+        /// <returns></returns>
+        public static AMO.Database ReadFromFile(string FileLocation)
+        {
+            using (System.Xml.XmlReader Reader = System.Xml.XmlReader.Create(FileLocation))
+            {
+                Reader.ReadToFollowing("Database");
+                return (AMO.Database)AMO.Utils.Deserialize(Reader, new AMO.Database());
+            }
+        }
+
+        /// <summary>
+        /// Writes an AMO database to the specified location
+        /// </summary>
+        /// <param name="FileLocation">The location to write the file to</param>
+        /// <param name="Database">The database to serialise</param>
+        public static void WriteToFile(string FileLocation, AMO.Database Database)
+        {
+            System.IO.File.WriteAllText(FileLocation, GetSerialisedXMLString(Database));
+        }
+        
+        public static string GetSerialisedXMLString(AMO.Database AMODatabase)
+        {
+            using (var StringWriter = new System.IO.StringWriter())
+            {
+                using (var XMLWriter = System.Xml.XmlWriter.Create(StringWriter, new System.Xml.XmlWriterSettings{
+                    Indent = true,
+                    IndentChars = "\t",
+                    NewLineOnAttributes = true
+                }))
+                {
+                    AMO.Utils.Serialize(XMLWriter, AMODatabase, false);
+                }
+                return StringWriter.ToString();
+            }
+        }
+    }
+}
---
+++ TOMWrapper/TOM2AMO/TranslationHelper.cs
@@ -0,0 +1,75 @@
+﻿using AMO = Microsoft.AnalysisServices;
+using TOM = Microsoft.AnalysisServices.Tabular;
+namespace TOM2AMO
+{
+    public static class TranslationHelper
+    {
+        public static int GetLCIDFromCultureName(string Name)
+        {
+            return new System.Globalization.CultureInfo(Name).LCID;
+        }
+
+        public static string GetCultureNameFromLCID(int LCID)
+        {
+            return new System.Globalization.CultureInfo(LCID).Name;
+
+        }
+
+        public static AMO.Translation GetTranslation(TOM.Culture TOMCulture, TOM.MetadataObject TOMObject)
+        {
+            AMO.Translation AMOTranslation = new AMO.Translation
+            {
+                Language = GetLCIDFromCultureName(TOMCulture.Name)
+            };
+
+            //Caption (i.e. Name)
+            if (TOMCulture.ObjectTranslations[TOMObject, TOM.TranslatedProperty.Caption] != null)
+                AMOTranslation.Caption = TOMCulture.ObjectTranslations[TOMObject, TOM.TranslatedProperty.Caption].Value;
+
+            //Description
+            if (TOMCulture.ObjectTranslations[TOMObject, TOM.TranslatedProperty.Description] != null)
+                AMOTranslation.Description = TOMCulture.ObjectTranslations[TOMObject, TOM.TranslatedProperty.Description].Value;
+            
+            //Display Folder
+            if (TOMCulture.ObjectTranslations[TOMObject, TOM.TranslatedProperty.DisplayFolder] != null)
+                AMOTranslation.DisplayFolder = TOMCulture.ObjectTranslations[TOMObject, TOM.TranslatedProperty.DisplayFolder].Value;
+
+            // If translation has no properties, it does not exist. 
+            // As such, return null unless it has at least one valid property
+            if (AMOTranslation.Caption != null || AMOTranslation.Description != null || AMOTranslation.DisplayFolder != null)
+                return AMOTranslation;
+            else
+                return null;
+        }
+
+        /// <summary>
+        /// Adds a translation for the TOM object based on the supplied AMOTranslation. 
+        /// Creates the relevant culture if it does not exist.
+        /// </summary>
+        /// <param name="TOMDatabase">The TOM Database to add the Translation to</param>
+        /// <param name="TOMObject">The TOM Object the translation is being added for</param>
+        /// <param name="AMOTranslation">The AMO Translation being transferred to the TOM Database</param>
+        public static void AddTOMTranslation(TOM.Database TOMDatabase, TOM.MetadataObject TOMObject, AMO.Translation AMOTranslation)
+        {
+            string CultureName = TranslationHelper.GetCultureNameFromLCID(AMOTranslation.Language);
+
+            //Add Culture if it does not exist
+            if (!TOMDatabase.Model.Cultures.ContainsName(CultureName))
+                TOMDatabase.Model.Cultures.Add(new TOM.Culture { Name = CultureName });
+
+            //Get existing culture
+            TOM.Culture TOMCulture = TOMDatabase.Model.Cultures.Find(CultureName);
+
+            //Add the various translated properties to the translation
+            TOMCulture.ObjectTranslations.Add(
+                new TOM.ObjectTranslation { Object = TOMObject, Property = TOM.TranslatedProperty.Caption, Value = AMOTranslation.Caption }
+            );
+            TOMCulture.ObjectTranslations.Add(
+                new TOM.ObjectTranslation { Object = TOMObject, Property = TOM.TranslatedProperty.Description, Value = AMOTranslation.Description }
+            );
+            TOMCulture.ObjectTranslations.Add(
+                new TOM.ObjectTranslation { Object = TOMObject, Property = TOM.TranslatedProperty.DisplayFolder, Value = AMOTranslation.DisplayFolder }
+            );
+        }
+    }
+}
